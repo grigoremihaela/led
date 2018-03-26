4862cfcc233ac758d11d9baef28944b9264d1835
grigoremihaela <grigoremihaela@ymail.com>
2018-03-27 00:44:33 +0300

    control variable

--- led.py
+++ led.py
@@ -20,7 +20,11 @@ statusPin = StringVar()
 labelPin = Label(root, textvariable=statusPin, fg='white', bg='black', font=labelFont)
 labelPin.place(x=70,y=-5)
 
+control = IntVar()
+control.set(0)
+
 countPin = IntVar()
+countPin.set(0)
 labelCount = Label(root, textvariable=countPin, fg='white', bg='black', font=labelFont)
 labelCount.place(x=70,y=-20)
 
@@ -28,12 +32,15 @@ def update():
     global statusPin, root # you don't really need to declare these as global because doing it this way gets rid of the ambiguity
     if GPIO.input(7) :
         GPIO.output(11,GPIO.HIGH)
-        statusPin.set('pin high')
-        countPin.set(countPin.get() + 1)
-        print("ON ")
+        if control == 0 :
+            countPin.set(countPin.get() + 1)
+            control.set(1)
+            statusPin.set('pin high')
+            print("ON ")
     else :
         GPIO.output(11,GPIO.LOW)
         statusPin.set('pin low')
+        control.set(0)
         print("OFF ")
     
     root.update()
