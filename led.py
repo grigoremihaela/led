7ec24787b42f49bcfdc253353d4c3de377c6528f
grigoremihaela <grigoremihaela@ymail.com>
2018-03-26 23:29:58 +0300

    count pin high

--- led.py
+++ led.py
@@ -12,7 +12,8 @@ GPIO.output(11, GPIO.LOW)
 root = tkinter.Tk()
 # root.configure(background='black')
 
-statusPin = StringVar()
+# statusPin = StringVar()
+statusPin = IntVar()
 labelFont = ("URW Chancery L", 45, 'bold', 'italic')
 labelPin = Label(root, textvariable=statusPin, fg='white', bg='black', font=labelFont)
 labelPin.place(x=70,y=-5)
@@ -21,29 +22,16 @@ def update():
     global statusPin, root # you don't really need to declare these as global because doing it this way gets rid of the ambiguity
     if GPIO.input(7) :
         GPIO.output(11,GPIO.HIGH)
-        statusPin.set('pin high')
-        print("LED ON ")
+        # statusPin.set('pin high')
+        statusPin = statusPin + 1
+        print("ON ")
     else :
         GPIO.output(11,GPIO.LOW)
-        statusPin.set('pin low')
-        print("LED OFF ")
+        # statusPin.set('pin low')
+        print("OFF ")
     
     root.update()
-    root.after(500, update)
-
-def ledON():
-    if GPIO.input(7) :
-        GPIO.output(11,GPIO.HIGH)
-        #ledButton["text"] = "LEDON"
-        labelPin = Label(root, text = 'pin high')
-        labelPin.pack()
-        print("LED ON ")
-    else :
-        GPIO.output(11,GPIO.LOW)
-        #ledButton["text"] = "LEDOFF"
-        labelPin = Label(root, text = 'pin low')
-        labelPin.pack()
-        print("LED OFF ")
+    root.after(5, update)
 
 def exitProgram():
     print("Exit Button pressed")
@@ -57,12 +45,7 @@ root.geometry('700x380')
 exitButton  = Button(root, text = "Exit", command = exitProgram, height =2 , width = 6)
 exitButton.pack(side = BOTTOM)
 
-ledButton = Button(root, text = "LED-ON", command = ledON, height = 2, width =8 )
-ledButton.pack()
-
-exitButton.pack(side = BOTTOM)
-
-ledButton = Button(root, text = "LED-ON", command = ledON, height = 2, width =8 )
+ledButton = Button(root, text = "count pin hight", command = ledON, height = 2, width =8 )
 ledButton.pack()
 
 labelPin.pack()
