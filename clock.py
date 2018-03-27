34e8f522371f43621e7550beca4b8ca8929ff4af
grigoremihaela <grigoremihaela@ymail.com>
2018-03-27 17:43:46 +0300

    test clock

--- clock.py
+++ clock.py
@@ -13,25 +13,19 @@ def quit(*args):
     
 def show_time():
     # Get the time remaining until the event
-    remainder = datetime.datetime.now()
+    remainder = endTime - datetime.datetime.now()
     # remove the microseconds part
-    remainder = datetime.timedelta(microseconds=remainder.microseconds)
+    remainder = remainder - datetime.timedelta(microseconds=remainder.microseconds)
     # Show the time left
     txt.set(remainder)
     # Trigger the countdown after 1000ms
     root.after(1000, show_time)
 
-def exitProgram():
-    print("Exit Button pressed")
-    GPIO.cleanup()
-    root.quit()
-
 # Use tkinter lib for showing the clock
 root = Tk()
-# root.attributes("-fullscreen", True)
-root.geometry('700x380')
+root.attributes("-fullscreen", True)
 root.configure(background='black')
-# root.bind("x", quit)
+root.bind("x", quit)
 root.after(1000, show_time)
 
 # Set the end date and time for the countdown
@@ -41,8 +35,5 @@ fnt = font.Font(family='Helvetica', size=60, weight='bold')
 txt = StringVar()
 lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="green", background="black")
 lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
-
-exitButton  = Button(root, text = "Exit", command = exitProgram, height =2 , width = 6)
-exitButton.pack(side = BOTTOM)
 
 root.mainloop()
