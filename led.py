d90c21342d8d6e00a96d363a425cc9c832abcd35
grigoremihaela <grigoremihaela@ymail.com>
2018-03-27 14:16:37 +0300

    time in variation

--- led.py
+++ led.py
@@ -3,7 +3,6 @@ import tkinter
 from tkinter import *
 #  import sys
 import time
-from time import gmtime, strftime
 import RPi.GPIO as GPIO
 #set up GPIO using BCM numbering
 #GPIO.setmode(GPIO.BCM)
@@ -18,26 +17,28 @@ GPIO.output(11, GPIO.LOW)
 root = tkinter.Tk()
 # root.configure(background='black')
 labelFont = ("URW Chancery L", 45, 'bold', 'italic')
-statusPin = StringVar('')
-control = IntVar(value=0)
-countPin = IntVar(value=0)
-timeIn = IntVar(value=0)
-timeStart = time.gmtime()
-timeEnd = time.gmtime()
+statusPin = StringVar()
+control = IntVar()
+countPin = IntVar()
+timeIn = IntVar()
+timeStart = time.time()
+timeEnd = time.time()
 
 def update():
     global statusPin, countPin, control, timeStart, timeEnd, timeIn, root # you don't really need to declare these as global because doing it this way gets rid of the ambiguity
+    timeStart = time.time()
+    timeEnd = time.time()
     if GPIO.input(7) :
         if (control.get() == 0) :
             timeIn = timeEnd - timeStart
-            timeStart = time.gmtime()
+            timeStart = time.time()
             countPin.set(countPin.get() + 1)
             control.set(1)                 # has counted
         # else : here the control is 1, so countPin has counted
         GPIO.output(11,GPIO.HIGH)
         statusPin.set('pin high')
         print("ON ") 
-        timeEnd = time.gmtime()
+        timeEnd = time.time()
     else : 
         if (control.get() == 1) :
             GPIO.output(11,GPIO.LOW)
