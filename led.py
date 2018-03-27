ca0bf180adb485561034232c211570fc9ba75514
grigoremihaela <grigoremihaela@ymail.com>
2018-03-27 15:21:30 +0300

    test clock

--- led.py
+++ led.py
@@ -1,9 +1,12 @@
 # Library Imports
-import tkinter
+#  import tkinter
 from tkinter import *
-#  import sys
+from tkinter import ttk
+from tkinter import font
 import time
+import datetime
 from datetime import datetime
+#  import sys
 import RPi.GPIO as GPIO
 #set up GPIO using BCM numbering
 #GPIO.setmode(GPIO.BCM)
@@ -14,14 +17,17 @@ GPIO.setup(7, GPIO.IN)
 # GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(11, GPIO.OUT)
 GPIO.output(11, GPIO.LOW)
-
-root = tkinter.Tk()
+#  root = tkinter.Tk()
+root = Tk()
 # root.configure(background='black')
 labelFont = ("URW Chancery L", 45, 'bold', 'italic')
 statusPin = StringVar()
 control = IntVar()
 countPin = IntVar()
 timeIn = IntVar()
+
+global endTime 
+
 timeStart = datetime.now().replace(microsecond=0)
 timeEnd = datetime.now().replace(microsecond=0)
 
@@ -86,6 +92,23 @@ labelTimeIn.pack()
 
 update()
 # mainloop()
+def show_time():
+    # Get the time remaining until the event
+    remainder = endTime - datetime.datetime.now()
+    # remove the microseconds part
+    remainder = remainder - datetime.timedelta(microseconds=remainder.microseconds)
+    # Show the time left
+    txt.set(remainder)
+    # Trigger the countdown after 1000ms
+    root.after(1000, show_time)
+# Set the end date and time for the countdown
+endTime = datetime.datetime(2017, 9, 19, 9, 0, 0)
+
+fnt = font.Font(family='Helvetica', size=60, weight='bold')
+txt = StringVar()
+lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="green", background="black")
+lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+
 root.mainloop()
 
 
