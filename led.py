8c9547f464bca42bdffbfff2c94ade45a1e8bf8f
grigoremihaela <grigoremihaela@ymail.com>
2018-03-27 10:40:38 +0300

    control variable

--- led.py
+++ led.py
@@ -6,8 +6,6 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN)
-
-GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 GPIO.output(11, GPIO.LOW)
 
@@ -20,20 +18,29 @@ statusPin = StringVar()
 labelPin = Label(root, textvariable=statusPin, fg='white', bg='black', font=labelFont)
 labelPin.place(x=70,y=-5)
 
+control = IntVar()
+control.set(0)
+
 countPin = IntVar()
+countPin.set(0)
 labelCount = Label(root, textvariable=countPin, fg='white', bg='black', font=labelFont)
 labelCount.place(x=70,y=-20)
 
 def update():
     global statusPin, root # you don't really need to declare these as global because doing it this way gets rid of the ambiguity
-    if GPIO.input(7) :
-        GPIO.output(11,GPIO.HIGH)
-        statusPin.set('pin high')
-        countPin.set(countPin.get() + 1)
-        print("ON ")
-    else :
+    if GPIO.input(7):
+            GPIO.output(11,GPIO.HIGH)
+            statusPin.set('pin high')
+            print("ON ") 
+        if control == 0:
+            countPin.set(countPin.get() + 1)
+            control.set(1)
+        else:
+            # here the control is 1, so countPin has counted
+    else: 
         GPIO.output(11,GPIO.LOW)
         statusPin.set('pin low')
+        control.set(0)
         print("OFF ")
     
     root.update()
@@ -55,6 +62,7 @@ ledButton = Button(root, text = "COUNT", height = 2, width =8 )
 ledButton.pack()
 
 labelCount.pack()
+
 labelPin.pack()
 
 update()
