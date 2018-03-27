fd8b757f9f69dc76a420e27637b823324b04302b
grigoremihaela <grigoremihaela@ymail.com>
2018-03-27 13:07:32 +0300

    time in variation

--- led.py
+++ led.py
@@ -29,6 +29,8 @@ countPin = IntVar()
 labelCount = Label(root, textvariable=countPin, fg='white', bg='black', font=labelFont)
 labelCount.place(x=70,y=-20)
 
+timeIn = time.time()
+
 def update():
     global statusPin, countPin, control, timeStart, timeEnd, timeIn, root # you don't really need to declare these as global because doing it this way gets rid of the ambiguity
     if GPIO.input(7) :
